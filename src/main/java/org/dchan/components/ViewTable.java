125b91a782b726651187d5eb1acf5e2d7005462c
Dchan <dulithaz@gmail.com>
2012-12-10 15:07:42 +0530

    Updated the code and fixed minor bug. Also added documentation

--- src/main/java/org/dchan/components/ViewTable.java
+++ src/main/java/org/dchan/components/ViewTable.java
@@ -1,10 +1,14 @@
 package org.dchan.components;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -18,19 +22,59 @@ import javax.swing.table.TableModel;
  *         table. Cache regions are not used since this is used for changing
  *         tables. But the fault of the table lies in complex queries that
  *         require multiple queries to access the data to be displayed.
+ * 
+ *         Example usage :-
+ * 
+ *         <pre>
+ * tblItems.setParameters(new String[] { &quot;SKUSKU&quot;, &quot;SKUDES&quot;, &quot;SKUSPR&quot; });
+ * tblItems.setConnection(Runner.getConnection());
+ * tblItems.setQuery(&quot;select  SKUSKU,SKUDES,SKUSPR  from SKUMAST&quot;);
+ * </pre>
  */
 @SuppressWarnings("serial")
 public class ViewTable extends JTable {
 	private Connection connection;
 	private String query;
-	private final String[] parameters;
+	private String[] parameters;
 	static DecimalFormat format = new DecimalFormat("0.00");
 
+	Map<String, Class> map = new HashMap<String, Class>();
+
+	public void put(String field, Class clas) {
+		map.put(field, clas);
+	}
+
+	public static void setFormat(DecimalFormat format) {
+		ViewTable.format = format;
+	}
+
+	/**
+	 * Use this constructor to later pass in the parameters later
+	 */
+	public ViewTable() {
+
+	}
+
+	/**
+	 * If we can provide the connection when the {@link ViewTable} object is
+	 * been created use this method
+	 * 
+	 * @param connection
+	 * @param query
+	 * @param parameters
+	 */
 	public ViewTable(Connection connection, String query, String parameters[]) {
 		this.connection = connection;
 		this.query = query;
 		this.parameters = parameters;
 		captureData();
+	}
+
+	/**
+	 * Covering method
+	 */
+	public void captureData() {
+		captureData(query, parameters);
 	}
 
 	/**
@@ -46,20 +90,31 @@ public class ViewTable extends JTable {
 		try {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
-
 			DefaultTableModel model = (DefaultTableModel) getModel();
-
 			while (resultSet.next()) {
 				String[] row = new String[parameters.length];
 				for (int i = 0; i < parameters.length; i++) {
 					String field = parameters[i];
-					Object object = resultSet.getObject(field);
-					if (object instanceof String) {
-						row[i] = object.toString();
-					} else if (object instanceof Double) {
-						row[i] = format.format(((Double) object));
-					} else if (object instanceof Integer) {
-						row[i] = ((Integer) object).toString();
+					try {
+						Object object = resultSet.getObject(field);
+						if (object instanceof String) {
+							row[i] = object.toString();
+						} else if (object instanceof Double) {
+							row[i] = format.format(((Double) object));
+						} else if (object instanceof Integer) {
+							row[i] = ((Integer) object).toString();
+						} else if (object instanceof BigDecimal) {
+							if (map.get(field) != null
+									&& map.get(field).equals(Integer.class)) {
+								int is = (int) ((BigDecimal) object)
+										.doubleValue();
+								row[i] = Integer.toString(is);
+							} else {
+								row[i] = format.format(((BigDecimal) object));
+							}
+						}
+					} catch (Exception e) {
+
 					}
 				}
 				model.addRow(row);
@@ -72,10 +127,18 @@ public class ViewTable extends JTable {
 	}
 
 	/**
-	 * Covering method
+	 * Print out the column names of the query
+	 * 
+	 * @throws SQLException
 	 */
-	private void captureData() {
-		captureData(query, parameters);
+	public void printColumnNames() throws SQLException {
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(query);
+		ResultSetMetaData metaData = resultSet.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		for (int i = 1; i <= columnCount; i++) {
+			System.out.println("Column name is = " + metaData.getColumnName(i));
+		}
 	}
 
 	/**
@@ -95,5 +158,20 @@ public class ViewTable extends JTable {
 		captureData();
 	}
 
-	
+	public void setParameters(String[] parameters) {
+		this.parameters = parameters;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	/**
+	 * Setup the connection after object creation
+	 * 
+	 * @param connection
+	 */
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 }
