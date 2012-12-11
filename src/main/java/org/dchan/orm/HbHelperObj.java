ad72bfd2200c960c0ee8b51e3a05b8965825e3aa
Dchan <dulithaz@gmail.com>
2012-12-11 10:23:16 +0530

    Added the constructor and body of startFactory() method

--- src/main/java/org/dchan/orm/HbHelperObj.java
+++ src/main/java/org/dchan/orm/HbHelperObj.java
@@ -1,10 +1,33 @@
 package org.dchan.orm;
 
+import org.hibernate.cfg.AnnotationConfiguration;
+
 public class HbHelperObj extends HbHelperAbstract {
+	private String url;
+	private String password;
+	private String schema;
+	private String user;
+	private boolean isProduction = false;
+
+	public HbHelperObj(String url, String user, String password, String schema) {
+		url = this.url;
+		password = this.password;
+		user = this.user;
+		schema = this.schema;
+	}
 
 	@Override
 	public void startFactory() {
-		
+		AnnotationConfiguration configuration = new AnnotationConfiguration()
+				.configure();
+		configuration.setProperty("hibernate.connection.url", url);
+		configuration.setProperty("hibernate.connection.username", user);
+		configuration.setProperty("hibernate.connection.password", password);
+		configuration.setProperty("hibernate.default_schema", schema);
+		if (isProduction) {
+			configuration.setProperty("hibernate.show_sql", "true");
+		}
+		factory = configuration.buildSessionFactory();
 	}
 
 }
